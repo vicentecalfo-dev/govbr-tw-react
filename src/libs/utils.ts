8546520c984fd4af917deaa5bd2f1bfc05a41fcd
Vicente Calfo <vicentecalfo@gmail.com>
2024-08-18 22:53:19 -0300

    feat: incrementar os ID do label for

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,10 +1,12 @@
 import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { uuid } from 'uuidv4'
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
   }
 
 export function getUIDClassName(){
-  return Math.floor(Math.random() * 10000).toString();
+  //return Math.floor(Math.random() * 10000).toString();
+  return uuid();
 }

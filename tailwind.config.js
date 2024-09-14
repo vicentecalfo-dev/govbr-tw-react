d7120502f9535ee3cd08441deef47ca32e3234b2
Vicente Calfo <vicentecalfo@gmail.com>
2024-09-13 21:40:33 -0300

    feat: Implementação da fonte oficial do governo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      'sans': 'Rawline Regular'
+    },
     extend: {
       colors: {
         govbr: {

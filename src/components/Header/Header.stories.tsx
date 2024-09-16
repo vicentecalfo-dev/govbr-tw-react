a51a8d1df6741391d77b9b65981489b904af7d86
Vicente Calfo <vicentecalfo@gmail.com>
2024-09-16 11:45:36 -0300

    feat: Inclusão do Cabeçalho.

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,21 +1,119 @@
 import React from "react";
 import { Header } from ".";
+import { Button } from "../Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCookieBite } from "@fortawesome/free-solid-svg-icons/faCookieBite";
+import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons/faCircleHalfStroke";
 
 export default {
   component: Header,
   title: "Header",
 };
 
-export const Default = () => (
-    <div className="w-full">
+const menu = [
+  {
+    "title": "Assuntos",
+    "link": "/menu1",
+    "subLinks": [
+      {
+        "title": "Notícias",
+        "link": "/menu2"
+      },
+      {
+        "title": "Biodiversidade e Biomas",
+        "link": "/menu1/sub1",
+        "subLinks": [
+          {
+            "title": "Áreas Protegidas",
+            "link": "/menu1/sub1/sub1"
+          },
+          {
+            "title": "Biodiversidade",
+            "link": "/menu1/sub1/sub1"
+          },
+          {
+            "title": "Direitos Animais",
+            "link": "/menu1/sub1/sub1"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "title": "Canais de Atendimento",
+    "link": "/menu3",
+    "subLinks": [
+      {
+        "title": "Endereços Importantes",
+        "link": "/menu3/sub1"
+      },
+      {
+        "title": "Ouvidoria",
+        "link": "/menu1/sub1",
+        "subLinks": [
+          {
+            "title": "Fala.BR",
+            "link": "/menu1/sub1/sub1"
+          },
+          {
+            "title": "Fale Conosco",
+            "link": "/menu1/sub1/sub1"
+          },
+          {
+            "title": "Legislação",
+            "link": "/menu1/sub1/sub1"
+          }
+        ]
+      }
+    ],
+  },
+  {
+    "type": "divisor"
+  },
+  {
+    "title": "Navegação",
+    "link": "/menu3",
+    "subLinks": [
+      {
+        "title": "Acessibilidade",
+        "link": "/menu3/sub1"
+      },
+      {
+        "title": "Mapa do Site",
+        "link": "/menu3/sub1"
+      },
+      {
+        "title": "Termo de Uso e Aviso de Privacidade",
+        "link": "/menu3/sub1"
+      }
+    ]
+  },
+]
 
-        <Header links={
-          [
-              {href:"",label:"Órgãos do Governo"},
-              {href:"",label:"Acesso à Informação"},
-              {href:"",label:"Legislação"},
-              {href:"",label:"Acessibilidade"}
-          ]
-        }/>
-    </div>
+
+export const Default = () => (
+  <div className="w-full">
+    <Header
+      locator="Ministério do Meio Ambiente e Mudança do Clima"
+      headerTitle="JBRJ/CNCFlora &bull; Centro Nacional de Conservação da Flora"
+      onChangeSearchInput={(value)=> console.log(value)}
+      onSubmitSearchInput={(value)=>console.log(value)}
+      menu={menu}
+    >
+      <Header.PrimaryMenu>
+        <a href="#">Órgãos do Governo</a>
+        <a href="#">Acesso à Informação</a>
+        <a href="#">Legislação</a>
+        <a href="#">Acessibilidade</a>
+      </Header.PrimaryMenu>
+      <Header.IconMenu>
+        <Button variant="ghost" size="icon">
+          <FontAwesomeIcon icon={faCookieBite} />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <FontAwesomeIcon icon={faCircleHalfStroke} />
+        </Button>
+      </Header.IconMenu>
+    </Header>
+  </div>
 );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,63 +1,237 @@
-import { ComponentProps, FC } from "react";
+import React, { ComponentProps, useState } from "react";
 import { GovBRLogo } from "../GovBRLogo";
 import { Button } from "../Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { cn, getUIDClassName } from "../../libs/utils";
 import {
-  faCircleHalfStroke,
-  faCookieBite,
+  faBars,
+  faChevronDown,
+  faChevronRight,
+  faMagnifyingGlass,
   faUser,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
+import Input from "../Input";
 
-interface HeaderProps extends ComponentProps<"header"> {
-  links?: Array<any>;
+interface MenuItem {
+  title?: string;
+  link?: string;
+  subLinks?: MenuItem[];
+  type?: string
 }
 
-const Header: FC<HeaderProps> = ({ children, className, links=[] }) => {
-  return (
-    <header className="grid grid-rows-[auto_auto] w-full">
-      <div className="flex gap-3  items-center">
-        <div className="w-[110px]">
-          <GovBRLogo />
-        </div>
-        <div className="flex-1"></div>
-        <div>
-          <ul className="flex gap-3 items-center">
-            {links.map(({ label, href }: any) => (
-              <a href={href} className="text-govbr-blue-warm-vivid-70 hover:text-govbr-blue-warm-vivid-80">{label}</a>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <ul className="flex gap-0">
-            <li>
-              <Button variant="ghost" size="icon">
-                <FontAwesomeIcon icon={faCookieBite} />
-              </Button>
-            </li>
-            <li>
-              <Button variant="ghost" size="icon">
-                <FontAwesomeIcon icon={faCircleHalfStroke} />
-              </Button>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <Button>
-            <FontAwesomeIcon icon={faUser} />
-            Entrar com gov.br
-          </Button>
-        </div>
-      </div>
-      <div>
-        <div>
+interface HeaderProps extends ComponentProps<"header"> {
+  locator?: string;
+  headerTitle?: string;
+  login?: boolean;
+  loginLabel?: string;
+  onChangeSearchInput?: any;
+  onSubmitSearchInput?: any;
+  menu?: MenuItem[];
+}
 
+const Header = ({
+  children,
+  locator,
+  headerTitle,
+  login = true,
+  loginLabel = "Entrar com gov.br",
+  onChangeSearchInput,
+  onSubmitSearchInput,
+  menu = [],
+}: HeaderProps) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [menuVisibility, setMenuVisibility] = useState(false);
+
+  // Estado para rastrear item selecionado e sublink selecionado
+  const [selectedMenuIndex, setSelectedMenuIndex] = useState<number | null>(
+    null
+  );
+  const [selectedSubMenuIndex, setSelectedSubMenuIndex] = useState<
+    number | null
+  >(null);
+
+  function handleOnChangeSearchInput(event: any) {
+    const value = event.target.value;
+    onChangeSearchInput(value);
+    setSearchValue(value);
+  }
+
+  function handleMenuMouseLeave() {
+    setSelectedMenuIndex(null);
+    setSelectedSubMenuIndex(null);
+  }
+
+  return (
+    <>
+      <header className="grid grid-rows-[auto_auto] w-full gap-6 py-6">
+        <div className="flex gap-3  items-center">
+          <div className="flex gap-6 items-center">
+            <div className="w-[110px]">
+              <GovBRLogo />
+            </div>
+            {locator && (
+              <h1 className="text-xs text-govbr-gray-80 max-w-[200px]">
+                {locator}
+              </h1>
+            )}
+          </div>
+
+          <div className="flex-1"></div>
+          {children}
+          {login && (
+            <div>
+              <Button>
+                <FontAwesomeIcon icon={faUser} />
+                {loginLabel}
+              </Button>
+            </div>
+          )}
         </div>
-        <div>
-            
+        <div className="flex gap-6">
+          <div className="ml-[-10px] flex gap-2 items-center w-3/4">
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => setMenuVisibility(!menuVisibility)}
+            >
+              <FontAwesomeIcon icon={menuVisibility ? faXmark : faBars} />
+            </Button>
+            {headerTitle && (
+              <h1 className="text-xl text-govbr-gray-80 p-0 m-0">
+                {headerTitle}
+              </h1>
+            )}
+          </div>
+          <div className="w-1/4">
+            <Input
+              placeholder="O que você procura?"
+              type="text"
+              iconPosition="right"
+              variant="featured"
+              density="lowest"
+              className="w-full"
+              value={searchValue}
+              onChange={handleOnChangeSearchInput}
+            >
+              <Button
+                size="icon"
+                variant="ghost"
+                density="high"
+                onClick={() => onSubmitSearchInput(searchValue)}
+              >
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </Button>
+            </Input>
+          </div>
         </div>
+      </header>
+      <div className="relative">
+        {menuVisibility && (
+          <div
+            className="bg-govbr-gray-10 w-full min-h-screen border-t-4 border-govbr-yellow-vivid-20 absolute flex gap-0"
+            onMouseLeave={handleMenuMouseLeave}
+          >
+            {/* Primeira coluna: Menu principal */}
+            <ul className="bg-transparent w-1/3">
+              {menu.map(({ title, link, subLinks, type }, index) => type? <li className=" py-6">
+                <hr className="border-t border-govbr-gray-20"/>
+              </li>:(
+                <li key={index}>
+                  <a
+                    href={link}
+                    className={cn("flex gap-3 p-3 text-govbr-gray-60 hover:bg-govbr-blue-warm-vivid-80 hover:text-govbr-pure-0", selectedMenuIndex === index ? `!bg-govbr-blue-warm-vivid-80 !text-govbr-pure-0`: "")}
+                    onMouseEnter={() => setSelectedMenuIndex(index)} // Rastreia qual item de menu está selecionado
+                  >
+                    <span className="flex-1">{title}</span>
+                    {subLinks && (
+                      <span>
+                        <FontAwesomeIcon icon={faChevronRight} />
+                      </span>
+                    )}
+                  </a>
+                </li>
+              ))}
+            </ul>
+
+            {/* Segunda coluna: Submenu (se houver) */}
+            {selectedMenuIndex !== null && menu[selectedMenuIndex]?.subLinks && (
+              <ul className="bg-govbr-pure-0/50 w-1/3 border-l border-govbr-gray-20">
+                {menu[selectedMenuIndex]?.subLinks!.map((subLink, index) => (
+                  <li key={index}>
+                    <a
+                      href={subLink.link}
+                      className={cn("flex gap-3 p-3 text-govbr-gray-60 hover:bg-govbr-blue-warm-vivid-70 hover:text-govbr-pure-0", selectedSubMenuIndex === index ? `!bg-govbr-blue-warm-vivid-70 !text-govbr-pure-0`: "")}
+                      onMouseEnter={() => setSelectedSubMenuIndex(index)} // Rastreia qual sublink está selecionado
+                    >
+                      <span className="flex-1">{subLink?.title}</span>
+                      {subLink?.subLinks && (
+                        <span>
+                          <FontAwesomeIcon icon={faChevronRight} />
+                        </span>
+                      )}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+
+            {/* Terceira coluna: Submenu do submenu (se houver) */}
+            {selectedMenuIndex !== null &&
+              selectedSubMenuIndex !== null &&
+              menu[selectedMenuIndex]?.subLinks &&
+              menu[selectedMenuIndex]?.subLinks![selectedSubMenuIndex]
+                ?.subLinks && (
+                <ul className="bg-govbr-pure-0/70 w-1/3 border-l border-govbr-gray-20">
+                  {menu[selectedMenuIndex]?.subLinks![
+                    selectedSubMenuIndex
+                  ]?.subLinks!.map((subSubLink, index) => (
+                    <li key={index}>
+                      <a
+                        href={subSubLink?.link}
+                        className="flex gap-3 p-3 text-govbr-gray-60 hover:bg-govbr-blue-warm-vivid-60 hover:text-govbr-pure-0"
+                      >
+                        {subSubLink?.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              )}
+          </div>
+        )}
       </div>
-    </header>
+    </>
   );
 };
+
+const HeaderPrimaryMenu = ({ children, className }: any) => {
+  const childrenArray = React.Children.toArray(children);
+  return (
+    <ul className="flex gap-3">
+      {childrenArray.map((child) => (
+        <li
+          key={getUIDClassName()}
+          className="text-govbr-blue-warm-vivid-70 hover:text-govbr-blue-warm-vivid-80"
+        >
+          {child}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const HeaderIconMenu = ({ children, className }: any) => {
+  const childrenArray = React.Children.toArray(children);
+  return (
+    <ul className="flex gap-0">
+      {childrenArray.map((child) => (
+        <li key={getUIDClassName()}>{child}</li>
+      ))}
+    </ul>
+  );
+};
+
+Header.PrimaryMenu = HeaderPrimaryMenu;
+Header.IconMenu = HeaderIconMenu;
 
 export { Header };

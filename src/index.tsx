47f3454d7bd54ce8b6ea95c287eceaa6e4626f46
= <=>
2026-02-24 14:04:02 -0300

    fix: exportação dos componentes

--- src/index.tsx
+++ src/index.tsx
@@ -1,142 +1,109 @@
-import Accordion from "./components/Accordion";
-import { Avatar } from "./components/Avatar";
-import { AvatarGroup } from "./components/AvatarGroup";
-import { Persona } from "./components/Persona";
-import { Rating } from "./components/Rating";
-import { SearchBox } from "./components/SearchBox";
-import { Tree } from "./components/Tree";
-import Table from "./components/Table";
-import Badge from "./components/Badge";
-import Breadcrumb from "./components/Breadcrumb";
-import { Button } from "./components/Button";
-import { ButtonGroup } from "./components/ButtonGroup";
-import { DatePicker } from "./components/DatePicker";
-import { CopyAction } from "./components/CopyAction";
-import CarouselSlider from "./components/CarouselSlider";
-import Item from "./components/Item";
-import ItemGroup from "./components/ItemGroup";
-import Card from "./components/Card";
-import { Carousel } from "./components/Carousel";
-import Checkbox from "./components/Checkbox";
-import Dialog from "./components/Dialog";
-import FeaturedTitle from "./components/FeaturedTitle";
-import FlyOut from "./components/FlyOut";
-import { GovBRLogo } from "./components/GovBRLogo";
-import { GovLogo } from "./components/GovLogo";
-import { Header } from "./components/Header";
-import Input from "./components/Input";
-import InputFile from "./components/InputFile";
-import { IucnCategory } from "./components/IucnCategory";
-import Kbd from "./components/Kbd";
-import List from "./components/List";
-import Login from "./components/Login";
-import Message from "./components/Message";
-import { MultiComboBox } from "./components/MultiComboBox";
-import OtpInput from "./components/OtpInput";
-import NativeSelect from "./components/NativeSelect";
-import PhotoGallery from "./components/PhotoGallery";
-import ProgressBar from "./components/ProgressBar";
-import Radio from "./components/Radio";
-import GroupOptions from "./components/GroupOptions";
-import Sheet from "./components/Sheet";
-import Skeleton from "./components/Skeleton";
-import Slider from "./components/Slider";
-import { Step } from "./components/Step";
-import {
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuPortal,
-    DropdownMenuRadioGroup,
-    DropdownMenuRadioItem,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuSub,
-    DropdownMenuSubContent,
-    DropdownMenuSubTrigger,
-    DropdownMenuTrigger
-} from "./components/DropdownMenu";
-import { Spinner } from "./components/Spinner";
-import Status from "./components/Status";
-import Switch from "./components/Switch";
-import Tabs from "./components/Tabs";
-import TextEditor, { DEFAULT_LABELS_PT_BR } from "./components/TextEditor";
-import Tooltip from "./components/Tooltip";
-import Calendar from "./components/Calendar";
-import Toaster from "./components/Toaster";
+export { default as Accordion } from "./components/Accordion";
+export { default as Badge } from "./components/Badge";
+export { default as Breadcrumb } from "./components/Breadcrumb";
+export { default as Calendar } from "./components/Calendar";
+export { default as Card } from "./components/Card";
+export { default as Checkbox } from "./components/Checkbox";
+export { default as Dialog } from "./components/Dialog";
+export { default as FeaturedTitle } from "./components/FeaturedTitle";
+export { default as FlyOut } from "./components/FlyOut";
+export { default as GroupOptions } from "./components/GroupOptions";
+export { default as Input } from "./components/Input";
+export { default as InputFile } from "./components/InputFile";
+export { default as Item } from "./components/Item";
+export { default as ItemGroup } from "./components/ItemGroup";
+export { default as Kbd } from "./components/Kbd";
+export { default as List } from "./components/List";
+export { default as Login } from "./components/Login";
+export { default as Message } from "./components/Message";
+export { default as NativeSelect } from "./components/NativeSelect";
+export { default as OtpInput } from "./components/OtpInput";
+export { default as ProgressBar } from "./components/ProgressBar";
+export { default as Radio } from "./components/Radio";
+export { default as Sheet } from "./components/Sheet";
+export { default as Skeleton } from "./components/Skeleton";
+export { default as Slider } from "./components/Slider";
+export { default as Status } from "./components/Status";
+export { default as Switch } from "./components/Switch";
+export { default as Table } from "./components/Table";
+export { default as Tabs } from "./components/Tabs";
+export { default as TextEditor } from "./components/TextEditor";
+export { default as Toaster } from "./components/Toaster";
+export { default as Tooltip } from "./components/Tooltip";
+
+export * from "./components/Avatar";
+export * from "./components/AvatarGroup";
+export * from "./components/Button";
+export * from "./components/ButtonGroup";
+export * from "./components/Carousel";
+export * from "./components/CarouselSlider";
+export * from "./components/CopyAction";
+export * from "./components/DatePicker";
+export * from "./components/DropdownMenu";
+export * from "./components/GovBRLogo";
+export * from "./components/GovLogo";
+export * from "./components/Header";
+export * from "./components/IucnCategory";
+export * from "./components/MultiComboBox";
+export * from "./components/Persona";
+export * from "./components/PhotoGallery";
+export * from "./components/Rating";
+export * from "./components/SearchBox";
+export * from "./components/Spinner";
+export * from "./components/Step";
+export * from "./components/Tree";
+
+export * from "./components/Calendar";
+export * from "./components/Input";
+export * from "./components/InputFile";
+export * from "./components/Item";
+export * from "./components/ItemGroup";
+export * from "./components/Kbd";
+export * from "./components/Login";
+export * from "./components/ProgressBar";
+export * from "./components/Skeleton";
+export * from "./components/Slider";
+export * from "./components/Table";
+export * from "./components/TextEditor";
+export * from "./components/Toaster";
 
 export {
-    Accordion,
-    Avatar,
-    AvatarGroup,
-    Persona,
-    Rating,
-    SearchBox,
-    Tree,
-    Table,
-    Badge,
-    Breadcrumb,
-    Button,
-    ButtonGroup,
-    DatePicker,
-    CopyAction,
-    CarouselSlider,
-    Item,
-    ItemGroup,
-    Card,
-    Checkbox,
-    Dialog,
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuCheckboxItem,
-    DropdownMenuRadioItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuGroup,
-    DropdownMenuPortal,
-    DropdownMenuRadioGroup,
-    DropdownMenuSub,
-    DropdownMenuSubTrigger,
-    DropdownMenuSubContent,
-    FeaturedTitle,
-    FlyOut,
-    GovBRLogo,
-    Input,
-    InputFile,
-    IucnCategory,
-    Kbd,
-    List,
-    Login,
-    Message,
-    MultiComboBox,
-    OtpInput,
-    NativeSelect,
-    PhotoGallery,
-    ProgressBar,
-    Radio,
-    GroupOptions,
-    Sheet,
-    Skeleton,
-    Slider,
-    Step,
-    Spinner,
-    Status,
-    Switch,
-    Tabs,
-    TextEditor,
-    DEFAULT_LABELS_PT_BR,
-    Tooltip,
-    Carousel,
-    GovLogo,
-    Header,
-    Calendar,
-    Toaster
-}
+  SIDEBAR_WIDTH,
+  SIDEBAR_WIDTH_ICON,
+  SIDEBAR_WIDTH_MOBILE,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInset,
+  SidebarInput,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSkeleton,
+  SidebarMenuSub,
+  SidebarMenuSubAction,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarOverlay,
+  SidebarProvider,
+  SidebarRail,
+  SidebarSeparator,
+  SidebarTrigger,
+  useSidebar,
+} from "./components/Sidebar";
 
-export * from "./components/Sidebar";
+export type {
+  SidebarMenuButtonProps,
+  SidebarMenuItemProps,
+  SidebarMenuSubButtonProps,
+  SidebarProps,
+  SidebarProviderProps,
+  SidebarTriggerProps,
+} from "./components/Sidebar";
